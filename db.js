c1cc8cb912aa4ddcec7f4c403b364714f0e63447
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2024-07-27 17:05:49 -0500

    menu10

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
 });
 
 module.exports = sequelize;
